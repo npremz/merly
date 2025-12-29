624c9752b574b8765cde96933a1eb772f9fff034
nico <ni.premont@gmail.com>
2025-12-29 13:12:45 +0100

    fix: compose

--- astro.config.mjs
+++ astro.config.mjs
@@ -10,7 +10,7 @@ import node from '@astrojs/node';
 // https://astro.build/config
 export default defineConfig({
   site: 'https://example.com', // TODO: Remplacer par l'URL du site
-  output: 'static',
+  output: 'server',
   adapter: node({ mode: 'standalone' }),
   server: {
     host: '0.0.0.0',
